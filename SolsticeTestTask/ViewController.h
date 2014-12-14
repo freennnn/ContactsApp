e3efc437a3e9b39abb0e9a0bad656408fec8938d
freennnnn <freennnn@gmail.com>
2014-12-14 17:55:25 -0600

    Initial Commit

---
+++ SolsticeTestTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SolsticeTestTask
+//
+//  Created by alexander.pranevich on 12/14/14.
+//  Copyright (c) 2014 AP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SolsticeTestTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SolsticeTestTask
+//
+//  Created by alexander.pranevich on 12/14/14.
+//  Copyright (c) 2014 AP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
